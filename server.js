173ac174dbd6e02c3dd80aeae956df780aa381db
marlonsalinas <msal_33@yahoo.com>
2021-09-30 13:43:35 -0500

    Tip calculator express application created

--- server.js
+++ server.js
@@ -8,8 +8,28 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`Hey there, ${req.params.name}!`);
 });
 
+// app.get('/tip/:total/:tipPercentage', tipAmount(req, res) => {
+//     const x = req.params.total
+
+// });
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+        let x = req.params.total;
+        let y = req.params.tipPercentage;
+        let tip = y / x * 100;
+        res.send(`${tip}`);
+    }
+);
 
 // Listen
 app.listen(PORT, () => {
     console.log('Listening on port:', PORT);
 })
+
+// let tipAmount (req.params.total, req.params.tipPercentage) {
+//     let x = req.params.total;
+//     let y = req.params.tipPercentage
+//     y = Math.pow(10, 2);
+//     let tip = x * y;
+// };
+// res.send(console.log(tip));
