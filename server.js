621d2d5b494d51df4425f83aef241732571064e4
marlonsalinas <msal_33@yahoo.com>
2021-09-30 13:51:28 -0500

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const express = require('express');
 const app = express();
 PORT = 3000;
 
+const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
 // Routes
 app.get('/greeting/:name', (req, res) => {
     res.send(`Hey there, ${req.params.name}!`);
 });
-
-// app.get('/tip/:total/:tipPercentage', tipAmount(req, res) => {
-//     const x = req.params.total
-
-// });
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
         let x = req.params.total;
@@ -21,15 +18,12 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     }
 );
 
+app.get('/magic/:question', (req, res) => {
+    res.send(responses[Math.floor(Math.random()*responses.length)]);
+});
+
+
 // Listen
 app.listen(PORT, () => {
     console.log('Listening on port:', PORT);
 })
-
-// let tipAmount (req.params.total, req.params.tipPercentage) {
-//     let x = req.params.total;
-//     let y = req.params.tipPercentage
-//     y = Math.pow(10, 2);
-//     let tip = x * y;
-// };
-// res.send(console.log(tip));
